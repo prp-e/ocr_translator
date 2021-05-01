f90ce79a14ad60dad8863d0e60588d4418a257d2
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-01 22:42:43 +0430

    working on detection

--- recognizer.py
+++ recognizer.py
@@ -14,9 +14,9 @@ for count, datum in enumerate(image_data.splitlines()):
         datum = datum.split()
         if len(datum) == 12:
             text = datum[11]
-            x,y,w,h = datum[6], datum[7], datum[8], datum[9]
+            x,y,w,h = int(datum[6]), int(datum[7]), int(datum[8]), int(datum[9])
             print(text)
-            cv2.rectangle(image, (x, y), (w+x, h+y), (255, 0, 85), 4)
-            
+            cv2.rectangle(image, (x,y), (w+x, h+y), (255, 0, 85), 5)
+
 cv2.imshow("image", image)
 cv2.waitKey(0)
