8c3a892893af036f8b68433e8bc821e9311377c0
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-11 16:43:29 +0430

    camera connection stablished

--- ocr_translate.py
+++ ocr_translate.py
@@ -25,3 +25,13 @@ import cv2
 import easyocr 
 
 reader = easyocr.Reader(['en'], gpu=False)
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    _, image = camera.read()
+
+    cv2.imshow('Camera', image)
+    exit = cv2.waitKey(30) & 0xff
+    if exit == 27:
+        break 
