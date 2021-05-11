4e182674fb49f94686098d83e98a21f79202cd00
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-11 17:25:18 +0430

    OCR Logic almost finished

--- ocr_translate.py
+++ ocr_translate.py
@@ -1,6 +1,9 @@
 """ Main code """ 
 
+from flask.globals import request
 import cv2 
+import json 
+import numpy as np
 import requests
 import time 
 
@@ -9,6 +12,10 @@ while True:
     _, image = camera.read()
     cv2.imshow('Camera', image)
     time.sleep(.5)
+    image = image.tolist()
+    #print(image)
+    image_data = {'image': image}
+    r = requests.post('http://localhost:5000/', json=image_data)
     exit = cv2.waitKey(30) & 0xff
     if exit == 27:
         break 
