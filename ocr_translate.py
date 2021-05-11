f68370925edd511a4f41d7b46245220c4348fc27
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-11 19:24:53 +0430

    Now works much better

--- ocr_translate.py
+++ ocr_translate.py
@@ -2,13 +2,14 @@
 
 from flask.globals import request
 import cv2 
+import easyocr
 import numpy as np
 import requests
 import sys 
 import time 
 
 camera = cv2.VideoCapture(0) 
-
+reader = easyocr.Reader(['en'], gpu=False)
 fps = camera.get(cv2.CAP_PROP_FPS)
 fps = int(fps) + 1
 print(f'FPS : {fps}')
@@ -17,6 +18,8 @@ while True:
     _, image = camera.read()
     cv2.imshow('Camera', image)
     cv2.imwrite('output.jpg', image)
+    image_data = reader.readtext('output.jpg')
+    print(image_data)
     time.sleep(.5)
     exit = cv2.waitKey(30) & 0xff
     if exit == 27:
