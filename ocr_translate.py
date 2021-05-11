b543ec204b6deb1e8e4d869f744c4c0704936e45
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-11 17:47:08 +0430

    Works good with laptops webcam

--- ocr_translate.py
+++ ocr_translate.py
@@ -2,19 +2,21 @@
 
 from flask.globals import request
 import cv2 
+import easyocr
 import json 
 import numpy as np
 import requests
 import time 
 
-camera = cv2.VideoCapture(0)
+reader = easyocr.Reader(['en'], gpu=False)
+camera = cv2.VideoCapture(1)
 while True:
     _, image = camera.read()
     cv2.imshow('Camera', image)
-    time.sleep(.5)
+    time.sleep(.15)
     #print(image)
-    image_data = {'image': image.tolist()}
-    r = requests.post('http://localhost:5000/', json=image_data)
+    image_data = reader.readtext(image)
+    print(image_data)
     exit = cv2.waitKey(30) & 0xff
     if exit == 27:
         break 
