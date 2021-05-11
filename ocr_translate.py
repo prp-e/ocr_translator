0d2fd03e11e848882ce656eb1e89eda771edc677
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-11 18:40:51 +0430

    Working on it, good results are comming

--- ocr_translate.py
+++ ocr_translate.py
@@ -6,21 +6,20 @@ import numpy as np
 import requests
 import time 
 
-reader = easyocr.Reader(['en'], gpu=False)
-camera = cv2.VideoCapture(1) 
+camera = cv2.VideoCapture(0) 
 
 fps = camera.get(cv2.CAP_PROP_FPS)
 fps = int(fps) + 1
 print(f'FPS : {fps}')
 
-
-def image_data(image):
-    print("This is a test!")
-
 while True:
     _, image = camera.read()
     cv2.imshow('Camera', image)
-    time.sleep(1/3.0)
+    time.sleep(.05)
+    image = image.tolist()
+    image_json = {'image': image}
+    r = requests.post('http://localhost:5000', json=image_json)
+    print(r)
     exit = cv2.waitKey(30) & 0xff
     if exit == 27:
         break 
