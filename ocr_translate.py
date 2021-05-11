d6f2ad1cb273e1f923c8d53e7f50dbf23dcedd6c
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-11 17:10:22 +0430

    Working on stuff

--- ocr_translate.py
+++ ocr_translate.py
@@ -1,15 +1,14 @@
 """ Main code """ 
 
 import cv2 
-import easyocr 
 import requests
+import time 
 
-reader = easyocr.Reader(['en'], gpu=False)
 camera = cv2.VideoCapture(0)
-
 while True:
     _, image = camera.read()
     cv2.imshow('Camera', image)
+    time.sleep(.5)
     exit = cv2.waitKey(30) & 0xff
     if exit == 27:
         break 
