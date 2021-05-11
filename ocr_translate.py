7e56111fe5e2ea8badb9a68a19987ecfa21a13c7
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-11 16:32:47 +0430

    SSL problem solved

---
+++ ocr_translate.py
@@ -0,0 +1,20 @@
+""" This code checks for macOS, if you have SSL problem with any other OS's, please add it to the code """
+import sys
+
+if sys.platform == 'darwin':
+    import requests 
+
+    requests.packages.urllib3.disable_warnings() 
+
+    import ssl 
+
+    try:
+
+        _create_unverified_https_context = ssl._create_unverified_context 
+
+    except AttributeError: 
+
+        pass 
+
+    else: 
+        ssl._create_default_https_context = _create_unverified_https_context
