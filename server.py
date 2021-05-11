ca7d5622f9adaa31593da1ac6aaa4a7a20dc899f
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-11 18:48:05 +0430

    A nice server for OCR operations

--- server.py
+++ server.py
@@ -34,7 +34,17 @@ def magic():
     json_input = request.get_json()
     image = np.array(json_input['image'])
     cv2.imwrite('output.jpg', image)
-    return {'output': 0}
+    input_image = cv2.imread('output.jpg')
+    image_data = reader.readtext(input_image)
+    
+    output_array = []
+    for datum in image_data:
+        output_data = {}
+        output_data['cooridnates'], output_data['text'], output_data['confidence'] = datum 
+        output_array.append(output_data)
+    
+    output = {'data': output_array}
+    return output
 
 if __name__ == '__main__':
     app.run(debug=True)
