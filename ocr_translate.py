250bbc2b83a2442d88a0bd5c61b8770980fea235
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-11 19:30:06 +0430

    A better showcase of the result

--- ocr_translate.py
+++ ocr_translate.py
@@ -16,11 +16,16 @@ print(f'FPS : {fps}')
 
 while True:
     _, image = camera.read()
-    cv2.imshow('Camera', image)
     cv2.imwrite('output.jpg', image)
     image_data = reader.readtext('output.jpg')
-    print(image_data)
-    time.sleep(.5)
+    for datum in image_data:
+        coordinates, text, confidence = datum 
+        top_left = (int(coordinates[0][0]), int(coordinates[0][1]))
+        bottom_right = (int(coordinates[2][0]), int(coordinates[2][1]))
+        cv2.rectangle(image, top_left, bottom_right, (0, 255, 0), 5)
+    
+    time.sleep(.75)
+    cv2.imshow('Camera', image)
     exit = cv2.waitKey(30) & 0xff
     if exit == 27:
         break 
