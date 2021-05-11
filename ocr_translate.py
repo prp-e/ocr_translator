dbe695ddd05c0133946b8e7219235242f67736e2
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-11 18:04:40 +0430

    Working on parallelism and threading

--- ocr_translate.py
+++ ocr_translate.py
@@ -3,20 +3,19 @@
 from flask.globals import request
 import cv2 
 import easyocr
-import json 
-import numpy as np
-import requests
+import threading
 import time 
 
 reader = easyocr.Reader(['en'], gpu=False)
-camera = cv2.VideoCapture(1)
+camera = cv2.VideoCapture(1) 
+
+def image_data(image):
+    print("This is a test!")
+
 while True:
     _, image = camera.read()
     cv2.imshow('Camera', image)
     time.sleep(.15)
-    #print(image)
-    image_data = reader.readtext(image)
-    print(image_data)
     exit = cv2.waitKey(30) & 0xff
     if exit == 27:
         break 
